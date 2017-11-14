19dbcf2b0bc2d83caf618d1ce325e967f90ae2a1
Itanor Strapazzon <itanor@gmail.com>
2017-11-13 22:37:48 -0200

    map to int

---
+++ capitulo7/Item7_6.java
@@ -0,0 +1,52 @@
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.OptionalInt;
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.toCollection;
+
+public class Item7_6 {
+
+  public static void main(String... args) {
+    List<Usuario> usuarios = new ArrayList<>();
+    usuarios.add(new Usuario("itanor", 30));
+    usuarios.add(new Usuario("pedro", 240));
+    usuarios.add(new Usuario("jose", 520));
+    usuarios.add(new Usuario("maria", 100));
+
+    List<Integer> pontos = usuarios
+      .stream()
+      .map(Usuario::getPontos)    // gera autoboxing...
+      .collect(toList());
+
+    pontos.forEach(System.out::println);
+
+    int[] arrayDePontos = usuarios
+      .stream()
+      .mapToInt(Usuario::getPontos)
+      .toArray();
+
+    for(int ponto: arrayDePontos) {
+      System.out.println(ponto);
+    }
+
+    double pontuacaoMedia = usuarios
+      .stream()
+      .mapToInt(Usuario::getPontos)
+      .average()
+      .getAsDouble();
+
+    System.out.println("Pontuação média: " + pontuacaoMedia);
+
+    OptionalInt pontuacaoMaxima = usuarios
+      .stream()
+      .mapToInt(Usuario::getPontos)
+      .max();
+
+    System.out.println("Pontuação máxima: " + pontuacaoMaxima.getAsInt());
+  }
+}
+
